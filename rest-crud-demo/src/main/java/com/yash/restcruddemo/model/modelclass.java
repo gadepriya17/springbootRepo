65e07320406970fdbb6e71b27f56e57afa0b2984
priya gade <gadepriya17@gmail.com>
2023-05-31 16:36:32 +0530

    adding getter setters

--- rest-crud-demo/src/main/java/com/yash/restcruddemo/model/modelclass.java
+++ rest-crud-demo/src/main/java/com/yash/restcruddemo/model/modelclass.java
@@ -1,4 +1,23 @@
 package com.yash.restcruddemo.model;
 
-public class modelclass {
+public class modelclass
+{
+    private String name;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
